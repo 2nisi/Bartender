6abd97bd485c431801cc712c005d809a31f3413e
Eris Sako <erissako@gmail.com>
2022-12-07 01:29:54 +0100

    Add files via upload

---
+++ bartender.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template
+from flask import request
+# import rospy
+
+# from rocoma_msgs.srv import SwitchController
+
+# switch_controller = rospy.ServiceProxy('/KinovaNoesisController/go_to_mode', SwitchController)
+# rospy.wait_for_service('/KinovaNoesisController/go_to_mode')
+print("Service available")
+
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(next(iter(request.form.keys())))
+        # switch_controller(next(iter(request.form.keys())))
+        #rospy.ServiceProxy('SwitchController', command)
+    buttons = [
+        {
+            'text': 'Go to Starting Position',
+            'command': 'home_pose',
+        },
+        {
+            'text': 'Sparkling Mint-Lime Iced Tea',
+            'command': 'command_B',
+        },
+        {
+            'text': 'Raspberry Frost Sodas',
+            'command': 'command_C',
+        },
+        {
+            'text': 'Pineapple Ginger Sparkler',
+            'command': 'command_D',
+        },
+        {
+            'text': 'Sparkling Pear Punch',
+            'command': 'command_E',
+        }       
+    ]
+    return render_template('main.html', buttons = buttons)
+
+if __name__ == '__main__':
+    app.run(host= '192.168.1.22')
---
+++ staticFiles/home.js
@@ -0,0 +1,24 @@
+//Global Variabels
+var OSName = "MacOS";
+
+
+
+//JavaScript functions of main page.
+
+//Reports the Operating System that the page is loaded
+function findOperatingSytem() {  
+   
+  if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows"; 
+  if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS"; 
+  if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX"; 
+  if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+    findOperatingSytem();
+    console.log(OSName);
+  });
