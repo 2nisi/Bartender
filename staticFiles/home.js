893837f6b6e511db4ea6f6a3c155823d9f5a124e
2nisi <erissako@gmail.com>
2023-11-27 00:08:11 +0100

    Fixed the website interface. Now all drinks can be served with any alchohol strength.

--- staticFiles/home.js
+++ staticFiles/home.js
@@ -26,10 +26,42 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-var slide = document.getElementById('alchoholStrength');
-
-slide.onchange = function() {
-
-      document.getElementById("drinkType").name = document.getElementById("drinkType").name + ";" + document.getElementById("alchoholStrength").value;
-      console.log(document.getElementById("alchoholStrength").value);
+var selected_drink = "";
+function update_drink_selection(selected_coctail){
+    console.log(selected_coctail);
+    selected_drink = selected_coctail;
 };
+
+
+const slider = document.getElementById('alchoholStrength');
+slider.oninput = function() {
+
+    var alchohol_strength = slider.value;
+    var alchohol_strength_text = "";
+
+    // Reseting all fonts to default
+    document.getElementById("lab_void").style.fontSize = "0.8rem";
+    document.getElementById("lab_soft").style.fontSize = "0.8rem";
+    document.getElementById("lab_medium").style.fontSize = "0.8rem";
+    document.getElementById("lab_strong").style.fontSize = "0.8rem";
+
+    // Depending on the alchohol strength value, the strength name is updated
+    if (alchohol_strength == 0) { alchohol_strength_text = "Void"; document.getElementById("lab_void").style.fontSize = "1.2rem";}
+    else if (alchohol_strength <= 45) { alchohol_strength_text = "Soft"; document.getElementById("lab_soft").style.fontSize = "1.2rem"; }
+    else if (alchohol_strength <= 85) { alchohol_strength_text = "Medium"; document.getElementById("lab_medium").style.fontSize = "1.2rem"; }
+    else if (alchohol_strength <= 100) { alchohol_strength_text = "Strong"; document.getElementById("lab_strong").style.fontSize = "1.2rem"; }
+
+    // Updating the shown strength name
+    document.getElementById("strength_text").innerHTML = 'Drink strength: <i> ' + alchohol_strength_text + '<i></h5>';
+};
+
+window.onload = function (){
+
+    document.getElementById("order_form").onsubmit = function(){
+
+        var order_drink = document.getElementById("submit_button");
+        order_drink.name = selected_drink + ";" + slider.value; // This is the text command that is send over Post method to ROS server
+    };
+
+};
+
